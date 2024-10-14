5c23f9536e6030b010e578750cf5a367271c8592
Clovis1444 <eu11111444@gmail.com>
2024-10-14 15:00:47 +0300

    add git support

--- lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
--- This module automatically imports all plugins from the current dir
--- DO NOT TOUCH
-
-return {
-  "folke/neodev.nvim",
-  "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
-}
