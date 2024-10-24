cb26c1db1ca42ba693312d7b8ef1449f74744621
Clovis1444 <eu11111444@gmail.com>
2024-10-24 17:49:46 +0300

    add <F3>: toggle relativenumber opt; set relativenumber on by default

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -2,11 +2,17 @@
 
 -- Functions to map
 function saveAndFormatFile()
-    vim.lsp.buf.format()
-    vim.cmd("write")
+	vim.lsp.buf.format()
+	vim.cmd("write")
+end
+
+function toggleRelativeNumber()
+	vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end
 
 -- Mapping
 vim.api.nvim_set_keymap("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("n", "<C-s>", ":lua saveAndFormatFile()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:lua saveAndFormatFile()<CR>a", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F3>", ":lua toggleRelativeNumber()<CR>", { noremap = true, silent = true })
