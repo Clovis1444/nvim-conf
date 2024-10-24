a6d04701d2153843579c98ee0673bcecc9a6e3cc
Clovis1444 <eu11111444@gmail.com>
2024-10-25 01:23:49 +0300

    add center on cursor after <C-d>/<C-u> mapping

--- lua/config/mapping.lua
+++ lua/config/mapping.lua
@@ -2,12 +2,12 @@
 
 -- Functions to map
 function saveAndFormatFile()
-	vim.lsp.buf.format()
-	vim.cmd("write")
+    vim.lsp.buf.format()
+    vim.cmd("write")
 end
 
 function toggleRelativeNumber()
-	vim.opt.relativenumber = not vim.opt.relativenumber:get()
+    vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end
 
 -- Mapping
@@ -16,3 +16,6 @@ vim.api.nvim_set_keymap("i", "<C-c>", "<Esc>", { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<C-s>", ":lua saveAndFormatFile()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:lua saveAndFormatFile()<CR>a", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<F3>", ":lua toggleRelativeNumber()<CR>", { noremap = true, silent = true })
+-- Center on cursor after scrolling
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
